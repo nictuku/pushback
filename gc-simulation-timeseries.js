4c97e18fd4d3b9c83c9c056d803796f0f5c62f0a
Yves Junqueira <yves.junqueira@gmail.com>
2016-11-30 16:12:27 -0800

    beautified

--- gc-simulation-timeseries.js
+++ gc-simulation-timeseries.js
@@ -12,34 +12,30 @@ var garbageMbpsVals = Array(xLength).fill(100);
 
 var elephant = 300;
 var cacheBloat = 4000;
-var crazyPeriods = [
-	 {
-      x: 50,
-		duration: 150,
-		inUseHeapMB: cacheBloat,
-	 },
-    {
-      x: 350,
-		duration: 100,
-		garbageMbps: elephant,
-	 },
-	{
-      x: 700,
-		duration: 150,
-		garbageMbps: elephant,
-		inUseHeapMB: cacheBloat,
-	 }
- ]
+var crazyPeriods = [{
+    x: 50,
+    duration: 150,
+    inUseHeapMB: cacheBloat,
+}, {
+    x: 350,
+    duration: 100,
+    garbageMbps: elephant,
+}, {
+    x: 700,
+    duration: 150,
+    garbageMbps: elephant,
+    inUseHeapMB: cacheBloat,
+}]
 
 for (c of crazyPeriods) {
-	for (i = c.x; i < c.x+c.duration; i++) {
-		if (c.garbageMbps) {
-			garbageMbpsVals[i] = c.garbageMbps;		
-		}
-		if (c.inUseHeapMB) {
-			inUseHeapMBVals[i] = c.inUseHeapMB;
-		}
-	}
+    for (i = c.x; i < c.x + c.duration; i++) {
+        if (c.garbageMbps) {
+            garbageMbpsVals[i] = c.garbageMbps;
+        }
+        if (c.inUseHeapMB) {
+            inUseHeapMBVals[i] = c.inUseHeapMB;
+        }
+    }
 }
 
 // These values are updated during the simulation.
@@ -50,95 +46,103 @@ var newGCTriggerVal = 1;
 
 
 var gcFunc = function(x) { // rename
-	//return goGC/100*x+x+extra;
-	var h = inUseHeapMBVals[x];
-	return goGC/100 * h + h;
+    //return goGC/100*x+x+extra;
+    var h = inUseHeapMBVals[x];
+    return goGC / 100 * h + h;
 }
 
 var newGcFunc = function(x) { // rename
-	var totalUsage = Math.min(memoryUsageNewVals[x] + extraMemoryVals[x], limit);
-	var h = inUseHeapMBVals[x];
-	var val = (1-totalUsage/limit) * goGC/100 * h + h;
-	console.log("x, h, total, val, limit", x, h, totalUsage, val, limit);
-	return val;
+    var totalUsage = Math.min(memoryUsageNewVals[x] + extraMemoryVals[x], limit);
+    var h = inUseHeapMBVals[x];
+    var val = (1 - totalUsage / limit) * goGC / 100 * h + h;
+    console.log("x, h, total, val, limit", x, h, totalUsage, val, limit);
+    return val;
 
 }
 
 var memLimit = {
-  x: timeXVals, 
-  y: timeXVals.map(i => limit),
-  line: {color: "rgb(255,0,0)"}, 
-  mode: "lines", 
-  name: "Container Memory Limit", 
-  type: "scatter"
+    x: timeXVals,
+    y: timeXVals.map(i => limit),
+    line: {
+        color: "rgb(255,0,0)"
+    },
+    mode: "lines",
+    name: "Container Memory Limit",
+    type: "scatter"
 };
 
 var extraMemory = {
-  x: timeXVals, 
-  y: extraMemoryVals,
-  line: {color: "rgb(0,0,255)"}, 
-  mode: "lines", 
-  name: "Non-heap Memory Usage", 
-  type: "scatter"
+    x: timeXVals,
+    y: extraMemoryVals,
+    line: {
+        color: "rgb(0,0,255)"
+    },
+    mode: "lines",
+    name: "Non-heap Memory Usage",
+    type: "scatter"
 };
 
 var memoryUsageFunc = function(x) {
-		if (x == 0) {
-		prev = 0
-	} else { 
-		prev = memoryUsageVals[x-1];
-	}
-	memoryUsageVals[x] = prev+garbageMbpsVals[x];
+    if (x == 0) {
+        prev = 0
+    } else {
+        prev = memoryUsageVals[x - 1];
+    }
+    memoryUsageVals[x] = prev + garbageMbpsVals[x];
 
-	 if (memoryUsageVals[x] > gcTriggerVal) {
-		gcTriggerVal = gcFunc(x);
-		console.log("GC!", x, memoryUsageVals[x], gcTriggerVal);
-		memoryUsageVals[x] = inUseHeapMBVals[x];
-	}
-	return memoryUsageVals[x] + extraMemoryVals[x];
+    if (memoryUsageVals[x] > gcTriggerVal) {
+        gcTriggerVal = gcFunc(x);
+        console.log("GC!", x, memoryUsageVals[x], gcTriggerVal);
+        memoryUsageVals[x] = inUseHeapMBVals[x];
+    }
+    return memoryUsageVals[x] + extraMemoryVals[x];
 }
 
 var memoryUsageNewFunc = function(x) {
-	if (x == 0) {
-		prev = 0
-	} else { 
-		prev = memoryUsageNewVals[x-1];
-	}
-	memoryUsageNewVals[x] = prev+garbageMbpsVals[x];
-	if (memoryUsageNewVals[x] > newGCTriggerVal) {
-		newGCTriggerVal = newGcFunc(x);
-		console.log("new GC!", x, memoryUsageNewVals[x], newGCTriggerVal);
-		memoryUsageNewVals[x] = inUseHeapMBVals[x];
-	}
-	return memoryUsageNewVals[x] + extraMemoryVals[x];
+    if (x == 0) {
+        prev = 0
+    } else {
+        prev = memoryUsageNewVals[x - 1];
+    }
+    memoryUsageNewVals[x] = prev + garbageMbpsVals[x];
+    if (memoryUsageNewVals[x] > newGCTriggerVal) {
+        newGCTriggerVal = newGcFunc(x);
+        console.log("new GC!", x, memoryUsageNewVals[x], newGCTriggerVal);
+        memoryUsageNewVals[x] = inUseHeapMBVals[x];
+    }
+    return memoryUsageNewVals[x] + extraMemoryVals[x];
 }
 
 var memoryUsage = {
-  x: timeXVals, 
-  y: timeXVals.map(memoryUsageFunc), 
-  line: {color: "gray"}, 
-  mode: "lines", 
-  name: "OldGC Memory usage",
-  type: "scatter"
+    x: timeXVals,
+    y: timeXVals.map(memoryUsageFunc),
+    line: {
+        color: "gray"
+    },
+    mode: "lines",
+    name: "OldGC Memory usage",
+    type: "scatter"
 };
 
 var memoryUsageNew = {
-  x: timeXVals, 
-  y: timeXVals.map(memoryUsageNewFunc), 
-  line: {color: "blue"}, 
-  mode: "lines", 
-  name: "NewGC Memory usage",
-  type: "scatter"
+    x: timeXVals,
+    y: timeXVals.map(memoryUsageNewFunc),
+    line: {
+        color: "blue"
+    },
+    mode: "lines",
+    name: "NewGC Memory usage",
+    type: "scatter"
 };
 
 console.log("memoryUsageNew", memoryUsageNew);
 
-var data = [ 
-	memoryUsage, memLimit, memoryUsageNew,
+var data = [
+    memoryUsage, memLimit, memoryUsageNew,
 ];
 
 var annotationFunc = function() {
-	/*
+    /*
 	{
       x: 600,
 		duration: 150,
@@ -146,60 +150,60 @@ var annotationFunc = function() {
 		inUseHeapMB: cacheBloat,
 	 }
 	 */
-	return crazyPeriods.map(function(p) {
-		var text = []
-		if (p.garbageMbps) {
-			text.push("High garbage gen")
-		}
-		if (p.inUseHeapMB) {
-			text.push("High InUse Mem")
-		}
-		return {
-			x: (p.x+p.duration/2),
-			y: 1000,
-      	xref: 'x',
-      	yref: 'y',
-      	text: text.join('<br>'),
-      	showarrow: false,
-      	// arrowhead: 1,
-      	ax: 0,
-      	ay: 40,
-			            bordercolor: "gray",
+    return crazyPeriods.map(function(p) {
+        var text = []
+        if (p.garbageMbps) {
+            text.push("High garbage gen")
+        }
+        if (p.inUseHeapMB) {
+            text.push("High InUse Mem")
+        }
+        return {
+            x: (p.x + p.duration / 2),
+            y: 1000,
+            xref: 'x',
+            yref: 'y',
+            text: text.join('<br>'),
+            showarrow: false,
+            // arrowhead: 1,
+            ax: 0,
+            ay: 40,
+            bordercolor: "gray",
             borderwidth: 1,
             borderpad: 4
-		}
-	})
+        }
+    })
 }
 var layout = {
-  paper_bgcolor: "rgb(255,255,255)", 
-  plot_bgcolor: "rgb(229,229,229)", 
-  xaxis: {
-    gridcolor: "rgb(255,255,255)", 
-    range: [timeXVals[0], timeXVals[-1]], 
-    showgrid: true, 
-    showline: false, 
-    showticklabels: true, 
-    tickcolor: "rgb(127,127,127)", 
-    ticks: "outside", 
-    zeroline: false,
-	 title: "Time",
-	 tick0: 0,
-    dtick: 20,
-  }, 
-  yaxis: {
-    gridcolor: "rgb(255,255,255)", 
-	 range: [0], // , 6000],
-    showgrid: true, 
-    showline: false, 
-    showticklabels: true, 
-    tickcolor: "rgb(127,127,127)", 
-    ticks: "outside", 
-    zeroline: false,
-	 title: "Peak Container Memory Usage",
-	 //tick0: 0,
-    //dtick: 1,
-  },
-	annotations: annotationFunc(),
+    paper_bgcolor: "rgb(255,255,255)",
+    plot_bgcolor: "rgb(229,229,229)",
+    xaxis: {
+        gridcolor: "rgb(255,255,255)",
+        range: [timeXVals[0], timeXVals[-1]],
+        showgrid: true,
+        showline: false,
+        showticklabels: true,
+        tickcolor: "rgb(127,127,127)",
+        ticks: "outside",
+        zeroline: false,
+        title: "Time",
+        tick0: 0,
+        dtick: 20,
+    },
+    yaxis: {
+        gridcolor: "rgb(255,255,255)",
+        range: [0], // , 6000],
+        showgrid: true,
+        showline: false,
+        showticklabels: true,
+        tickcolor: "rgb(127,127,127)",
+        ticks: "outside",
+        zeroline: false,
+        title: "Peak Container Memory Usage",
+        //tick0: 0,
+        //dtick: 1,
+    },
+    annotations: annotationFunc(),
 };
 
 Plotly.newPlot("myDiv", data, layout);
